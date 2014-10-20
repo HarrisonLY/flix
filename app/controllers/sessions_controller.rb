26ee09efd3c53f3355349fadb7609cc67b021969
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-10-20 12:29:29 -0400

    admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ def create
   if user = User.authenticate(params[:email], params[:password])
     session[:user_id] = user.id
     flash[:notice] = "Welcome back, #{user.name}!"
-    redirect_to user
+    redirect_to(session[:intended_url] || user)
+    session[:intended_url] = nil
   else
     flash.now[:alert] = "Invalid email/password combination!"
     render :new
@@ -18,5 +19,7 @@ def destroy
   session[:user_id] = nil
   redirect_to root_url, notice: "You're now signed out!"
 end
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020133725) do
+ActiveRecord::Schema.define(version: 20141020160109) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141020133725) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
